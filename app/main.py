15315d6f229cdebd0050a7ba67bd46969ace21b6
Freddy Rosa <freddy.rosa16@gmail.com>
2026-02-26 20:57:16 -0400

    Implemented full CRUD for Task Tracker API (Create, Read, Update, Delete)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Response
 from .database import Base, engine
 from . import models, schemas, database
 from pydantic import BaseModel
@@ -27,6 +27,33 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(database.get_db)
     return db_task
 
 @app.get("/tasks/", response_model=List[schemas.Task])
-def get_task(db: Session = Depends(database.get_db)):
+def get_tasks(db: Session = Depends(database.get_db)):
     tasks = db.query(models.Task).all()
     return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int, db: Session = Depends(database.get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
+
+@app.put("/tasks/{task_id}/", response_model=schemas.Task)
+def update_task(task_id: int, update_task: schemas.TaskCreate, db: Session = Depends(database.get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    task.title = update_task.title
+    task.description = update_task.description
+    db.commit()
+    db.refresh(task)
+    return task
+
+@app.delete("/tasks/{task_id}", response_model=schemas.Task)
+def delete_task(task_id: int, db: Session = Depends(database.get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db.delete(task)
+    db.commit()
+    return Response(status_code=204)
